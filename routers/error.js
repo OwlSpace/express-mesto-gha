3d624221efdb948234e52a33364a92cdaffebb09
Kristina <aspire9694@gmail.com>
2022-10-13 18:01:26 +0300

    Исправление ошибок

--- routers/error.js
+++ routers/error.js
@@ -1,6 +1,6 @@
 const errorRouter = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
 
-errorRouter.all('/', (req, res, next) => { return next(new NotFoundError('неправильный путь')) });
+errorRouter.all('/', (req, res, next) => next(new NotFoundError('неправильный путь')));
 
 module.exports = errorRouter;
